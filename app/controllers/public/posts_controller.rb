33f7e56b70108b3af4795ab0f2c41f25e75cf9cb
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-06-05 14:51:08 +0000

    [add]public/post new, index [add]ActiveStorage

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,13 +1,40 @@
 class Public::PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
-  end
-
-  def edit
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    @post.end_user_id = current_end_user.id
+    if @post.save
+      redirect_to posts_path
+      flash[:notice] = "投稿が完了しました"
+    else
+     render 'new'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+  private
+    def post_params
+      params.require(:post).permit(:view_image, :title, :body, :nation, :prefecture, :place)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+  has_one_attached :view_image
+  belongs_to :end_user
+
+  validates :view_image, presence: true
+  validates :title, presence: true
+  validates :nation, presence: true
+  validates :prefecture, presence: true
+  validates :place, presence: true
+
+  def get_view_image
+    if view_image.attached?
+      view_image
+    else
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+  end
 end
