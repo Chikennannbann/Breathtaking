6a4d8be173f0323b25ff32f00d979fa2179726a2
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-06-06 09:31:45 +0000

    [fix] routes/end_users [add] public/end_user edit show

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,6 +3,16 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
+  has_one_attached :profile_image
   has_many :posts, dependent: :destroy
+
+  def get_profile_image
+    if profile_image.attached?
+      profile_image
+    else
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,11 @@ Rails.application.routes.draw do
     root 'homes#top'
     get 'about' => 'homes#about', as: 'about'
 
-    get 'end_users/mypage' => 'end_users#show', as: 'mypage'
-    get 'end_users/information/edit' => 'end_users#edit', as: 'edit_information'
-    patch 'end_users/information' => 'end_users#update', as: 'update_information'
     get 'end_users/unsubscribe' => 'end_users#unsubscribe', as: 'confirm_unsubscribe'
     patch 'end_users/withdraw' => 'end_users#withdraw', as: 'withdraw_end_user'
     get 'search' => 'serches/search'
 
+    resources :end_users, only: [:show, :edit, :update]
     resources :posts do
       resources :post_comments, only: [:create, :destroy]
     end
