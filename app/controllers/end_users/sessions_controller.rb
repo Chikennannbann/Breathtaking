79f38470157fd207552f98d2fc67b60c7d19116a
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-06-07 08:48:38 +0000

    [add] guest_sign_in

---
+++ app/controllers/end_users/sessions_controller.rb
@@ -0,0 +1,8 @@
+class EndUsers::SessionsController < Devise::SessionsController
+  def guest_sign_in
+    end_user = EndUser.guest
+    sign_in end_user
+    redirect_to posts_path
+    flash[:notice] = "ゲストユーザーでログインしました"
+  end
+end
--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -15,4 +15,11 @@ class EndUser < ApplicationRecord
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
   end
+
+  def self.guest
+    find_or_create_by!(name: 'guestenduser', email: 'guest@example.com') do |end_user|
+      end_user.password = SecureRandom.urlsafe_base64
+      end_user.name = "ゲストユーザー"
+    end
+  end
 end
