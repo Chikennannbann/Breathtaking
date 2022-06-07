0719ccd79fc58285da046faadfc30deb055e841c
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-06-07 09:19:23 +0000

    [fix] model/Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,11 +9,6 @@ class Post < ApplicationRecord
   validates :place, presence: true
 
   def get_view_image
-    if view_image.attached?
       view_image
-    else
-      file_path = Rails.root.join('app/assets/images/no_image.jpg')
-      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-    end
   end
 end
