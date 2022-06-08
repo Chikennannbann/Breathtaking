c492a8c54eb4ed06342ae9d8a8c2210861293084
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-06-08 07:15:31 +0000

    [add] public/favorite [fix] public/post show

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :view_image
   belongs_to :end_user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :view_image, presence: true
   validates :title, presence: true
@@ -11,5 +12,9 @@ class Post < ApplicationRecord
 
   def get_view_image
       view_image
+  end
+
+  def favorited_by?(end_user)
+    favorites.exists?(end_user_id: end_user.id)
   end
 end
