0adc27fd14dfc33b9493f2dba0f18dc70c0efe18
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-06-21 14:17:09 +0000

    [fix] spec

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,38 @@
 
 require 'rails_helper'
 
-describe 'Postモデルのテスト' do
-  it '有効な投稿内容の場合は保存されるか' do
-    post.view_image = Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/images/test.jpg')).to be_valid
-    expect(FactoryBot.build(:post)).to be_valid
+RSpec.describe Post, 'モデルに関するテスト', type: :model do
+  describe '実際に保存してみる' do
+    it '有効な投稿内容の場合は保存されるか' do
+      #画像？
+      # post.view_image = Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/images/test.jpg'))
+      expect(FactoryBot.build(:post)).to be_valid
+    end
+  end
+  context '空白のバリデーションチェック' do
+    it 'titleが空白の倍いにバリデーションチェックされ空白のエラーメッセージが返ってきているか' do
+      post = Post.new(title:'', nation:'hoge', prefecture:'hoge', place:'hoge')
+      #画像？
+      expect(post).to be_invalid
+      expect(post.errors[:title]).to include("can't be blank")
+    end
+    it 'nationが空白の倍いにバリデーションチェックされ空白のエラーメッセージが返ってきているか' do
+      post = Post.new(title:'hoge', nation:'', prefecture:'hoge', place:'hoge')
+      #画像？
+      expect(post).to be_invalid
+      expect(post.errors[:nation]).to include("can't be blank")
+    end
+    it 'prefectureが空白の倍いにバリデーションチェックされ空白のエラーメッセージが返ってきているか' do
+      post = Post.new(title:'hoge', nation:'hoge', prefecture:'', place:'hoge')
+      #画像？
+      expect(post).to be_invalid
+      expect(post.errors[:prefecture]).to include("can't be blank")
+    end
+    it 'placeが空白の倍いにバリデーションチェックされ空白のエラーメッセージが返ってきているか' do
+      post = Post.new(title:'hoge', nation:'hoge', prefecture:'hoge', place:'')
+      #画像？
+      expect(post).to be_invalid
+      expect(post.errors[:place]).to include("can't be blank")
+    end
   end
 end
--- spec/system/post_spec.rb
+++ spec/system/post_spec.rb
@@ -1,3 +1,47 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
+describe '投稿のテスト' do
+  let!(:post) { create(:post, title:'hoge', nation:'hoge', prefecture:'hoge', place:'hoge') }
+  #画像？
+  describe 'トップ画面(root_path)のテスト' do
+    before do
+      visit root_path
+    end
+    context '表示の確認' do
+      it 'トップ画面(root_path)に「絶景好きが集まる」が表示されているか' do
+        expect(page).to have_content '絶景好きが集まる'
+      end
+      it 'root_pathが/であるか' do
+        expect(current_path).to eq('/')
+      end
+    end
+  end
+
+  describe '投稿画面(new_post_path)のテスト' do
+    before do
+      visit new_post_path
+    end
+    context '表示の確認' do
+      it 'new_post_pathが/posts/newであるか' do
+        expect(current_path).to eq('/posts/new')
+      end
+      it '投稿ボタンが表示されているか' do
+        expect(page).to have_button '投稿'
+      end
+    end
+    context '投稿処理のテスト' do
+      it '投稿後のリダイレクト先は正しいか' do
+        fill_in 'post[title]', with: Faker::Lerem.character(number:10)
+        fill_in 'post[nation]', with: Faker::Lerem.character(number:5)
+        fill_in 'post[prefecture]', with: Faker::Lerem.character(number:5)
+        fill_in 'post[place]', with: Faker::Lerem.character(number:10)
+        # expect(FactoryBot.build(:post))でいける？
+        #画像？
+        click_button '投稿'
+        expect(page).to have_current_path posts_path(Post.last)
+      end
+    end
+  end
+end
