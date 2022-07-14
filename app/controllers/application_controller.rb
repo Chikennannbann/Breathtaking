86167361993a464d5b35cfe20966dbf20e4cec00
Chikennannbann <102850169+Chikennannbann@users.noreply.github.com>
2022-07-14 14:47:06 +0000

    [fix] config/ja en.yml, layouts/application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+  before_action :generate_variable_path
 
   def set_locale
     I18n.locale = locale
@@ -7,15 +9,21 @@ class ApplicationController < ActionController::Base
   end
 
   def locale
+    # binding.pry
     @locale ||= params[:locale] ||= I18n.default_locale
     # パラメータで受け取った言語をインスタンス変数へ
     # パラメータがない場合はデフォルトの言語を使用(jaを設定済)
-
   end
 
   def default_url_options(options={})
     options.merge(locale: locale)
     # 画面遷移した時に言語の設定が消えないようにする
+  end
+
+  def generate_variable_path
+    @path = request.path.gsub(%r(/ja/|/en/), "")
+    # ページのURLをrequest.pathで受け取り、/enや/jaを削除。
+    # どのページから切り替えても多言語対応を可能にするため
   end
 
   protected
